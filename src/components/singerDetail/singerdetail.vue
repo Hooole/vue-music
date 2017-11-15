3da0b54311f92f3318d62f1c7752868768473c6a
fuhailiang <fh777l@163.com>
2017-11-15 18:36:46 +0800

    歌手详情裂变

--- src/components/singerDetail/singerdetail.vue
+++ src/components/singerDetail/singerdetail.vue
@@ -4,9 +4,31 @@
   </transition>
 </template>
 <script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+
   export default {
+    computed: {
+      ...mapGetters([
+        'singer'
+      ])
+    },
     created() {
-      console.log(this.$route.id)
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
+          }
+        })
+      }
     }
   }
 </script>
