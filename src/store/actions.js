f5fc706c4629078384c59560a55584816d57f248
fuhailiang <fh777l@163.com>
2017-12-01 18:05:33 +0800

    随机播放

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,33 @@
 import * as types from './mutation-types'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/util'
 
-export const selectPlay = function ({commit, play}, {list, index}) {
+function findIndex(list, songs) {
+  return list.findIndex((item) => {
+    return item.id === songs.id
+  })
+}
+
+export const selectPlay = function ({commit, state}, {list, index}) {
   commit(types.SET_PLAYING_STATE, true)
   commit(types.SET_FULL_SCREEN, true)
-  commit(types.SET_PLAY_LIST, list)
+  if (state.mode === playMode.random) {
+    let randomlist = shuffle(list)
+    commit(types.SET_PLAY_LIST, randomlist)
+    index = findIndex(randomlist, list[index])
+  } else {
+    commit(types.SET_PLAY_LIST, list)
+  }
   commit(types.SET_SEQUENCE_LIST, list)
   commit(types.SET_CURRENT_INDEX, index)
+}
+
+export const randomPlay = function ({commit}, {list, index}) {
+  commit(types.SET_PLAY_MODE, playMode.random)
+  commit(types.SET_PLAYING_STATE, true)
+  commit(types.SET_SEQUENCE_LIST, list)
+  let randomlist = shuffle(list)
+  commit(types.SET_PLAY_LIST, randomlist)
+  commit(types.SET_CURRENT_INDEX, 0)
+  commit(types.SET_FULL_SCREEN, true)
 }
